d06d30237d45991dec59dda1af4e4b61ed2ed24e
chakannom <genteelson@gmail.com>
2020-08-10 23:53:13 +0900

    Applied win32 app

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,127 @@
+#pragma comment (lib, "user32.lib")
+
 #include <iostream>
+#define WIN32_LEAN_AND_MEAN  // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+#include <windows.h>
 
 #include <core/config/interrupt/interrupt_handler.hpp>
 #include <core/util/runtime_util.hpp>
-
 #include <agent/apis/agent_apis_controller.hpp>
+
+#define IDC_START_BTN 101
+#define IDC_STOP_BTN  102
 
 using namespace web;
 using namespace cfx;
 using namespace cks;
 
-int main(int argc, wchar_t *argv[])
+HINSTANCE hInst;                                // 현재 인스턴스
+WCHAR *szTitle = L"TITLE";                      // 제목 표시줄 텍스트
+WCHAR *szWindowClass = L"CLASSNAME";            // 기본 창 클래스 이름
+AgentApisController agentApisController;
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
 {
-    InterruptHandler::hookSIGINT();
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-    AgentApisController agentApisController;
-    agentApisController.setEndpoint(L"http://host_auto_ip4:28080/agent/apis");
-    
-    try {
-        // wait for server initialization...
-        agentApisController.accept().wait();
-        std::wcout << "StockAgent now listening for requests at: " << agentApisController.endpoint() << std::endl;
+    MyRegisterClass(hInstance);
 
-        InterruptHandler::waitForUserInterrupt();
-
-        agentApisController.shutdown().wait();
-    }
-    catch(std::exception & e) {
-        std::wcerr << "somehitng wrong happen! :(" << std::endl;
-    }
-    catch(...) {
-        RuntimeUtil::printStackTrace();
+    // 애플리케이션 초기화 수행
+    if (!InitInstance(hInstance, nCmdShow)) {
+        return FALSE;
     }
 
+    MSG msg;
+
+    // 기본 메시지 루프
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
+}
+
+// 창 클래스를 등록
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(nullptr, IDI_APPLICATION);
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = nullptr;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(nullptr, IDI_APPLICATION);
+
+    return RegisterClassExW(&wcex);
+}
+
+// 인스턴스 핸들을 저장하고 주 창을 만듦
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance;
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd) {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+// 주 창의 메시지를 처리
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+        case WM_CREATE:
+            {
+                SendMessage(hWnd, WM_COMMAND, MAKEWPARAM(IDC_START_BTN, BN_CLICKED), (LPARAM)0);
+                SendMessage(hWnd, WM_COMMAND, MAKEWPARAM(IDC_STOP_BTN, BN_CLICKED), (LPARAM)0);
+            }
+            break;
+        case WM_COMMAND:
+            {
+                switch (LOWORD(wParam)) {
+                    case IDC_START_BTN:
+                        std::cout << "START" << std::endl;
+                        agentApisController.setEndpoint(L"http://host_auto_ip4:28080/agent/apis");
+                        agentApisController.accept().wait();
+                        std::wcout << L"StockAgent now listening for requests at: " << agentApisController.endpoint() << std::endl;
+                        break;
+                    case IDC_STOP_BTN:
+                        std::cout << "STOP" << std::endl;
+                        agentApisController.shutdown().wait();
+                        break;
+                }
+            }
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
     return 0;
 }
